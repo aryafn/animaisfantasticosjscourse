4964b88aacf912f5ab6c90d375b8a57dd59a441b
Ary Ferreira <aryafneto@gmail.com>
2023-01-13 16:25:56 -0300

    Primeiro commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+"module.exports" = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+  },
+  "extends": 'standard',
+  "overrides": [],
+  "parserOptions": {
+   " ecmaVersion": 'latest',
+    "sourceType": 'module',
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "import/extensions": 0,
+    "comma-dangle": 'off',
+  },
+};
